7823ffb2fd7381ddc4741ec8d84f59399c3e651d
ReallyLiri <reallyliri@gmail.com>
2021-08-13 15:27:45 +0300

    Adding apply button

---
+++ src/main/java/com/reallyliri/plugins/namespaceproviders/Applier.java
@@ -0,0 +1,169 @@
+package com.reallyliri.plugins.namespaceproviders;
+
+import com.google.common.collect.Streams;
+import com.intellij.openapi.diagnostic.Logger;
+import com.jetbrains.rider.model.RdCustomLocation;
+import com.jetbrains.rider.model.RdProjectDescriptor;
+import com.jetbrains.rider.model.RdProjectFolderDescriptor;
+import com.jetbrains.rider.model.RdSolutionDescriptor;
+import com.jetbrains.rider.projectView.workspace.ProjectModelEntity;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Applier {
+
+    private static final Logger log = Logger.getInstance(Applier.class);
+
+    private final Map<String, Set<String>> directoriesPathsByProject = new HashMap<>();
+    private final Set<String> ignoredDirectoryNames = Set.of(
+        ".idea",
+        "bin",
+        "bld",
+        "build",
+        "logs",
+        "obj",
+        "target"
+    );
+
+    public int apply(ProjectModelEntity parentNode, boolean applyFullTree) throws IOException {
+        return (parentNode.getDescriptor() instanceof RdSolutionDescriptor)
+            ? applyRdSln(parentNode, applyFullTree)
+            : applyNode(parentNode, false);
+    }
+
+    private int applyNode(ProjectModelEntity parentNode, boolean applyFullTree) throws IOException {
+        String projectPath = projectPath(parentNode);
+        if (projectPath == null) {
+            return 0;
+        }
+
+        List<ProjectModelEntity> nodes = new ArrayList<>();
+        collectChildFolders(parentNode, nodes, applyFullTree);
+
+        Set<String> nodePaths = nodes.stream()
+            .map(this::nodeFullRelativePath)
+            .collect(Collectors.toSet());
+
+        return apply(projectPath, nodePaths);
+    }
+
+    private void collectChildFolders(ProjectModelEntity parentNode, List<ProjectModelEntity> nodes, boolean recursive) {
+        List<ProjectModelEntity> children = Streams.stream(parentNode.getChildrenEntities().iterator())
+            .filter(node -> node.getDescriptor() instanceof RdProjectFolderDescriptor)
+            .filter(node -> !ignoredDirectoryNames.contains(node.getName().toLowerCase()))
+            .collect(Collectors.toList());
+        nodes.addAll(children);
+        if (!recursive) {
+            return;
+        }
+        for (ProjectModelEntity child : children) {
+            collectChildFolders(child, nodes, true);
+        }
+    }
+
+    private int applyRdSln(ProjectModelEntity slnNode, boolean applyFullTree) throws IOException {
+        List<ProjectModelEntity> rdProjects = Streams.stream(slnNode.getChildrenEntities().iterator())
+            .filter(node -> node.getDescriptor() instanceof RdProjectDescriptor)
+            .collect(Collectors.toList());
+
+        int updated = 0;
+        for (ProjectModelEntity projectNode : rdProjects) {
+            String projectPath = projectPath(projectNode);
+            if (projectPath == null) {
+                continue;
+            }
+            if (!directoriesPathsByProject.containsKey(projectPath)) {
+                directoriesPathsByProject.put(projectPath, new HashSet<>());
+            }
+            if (applyFullTree) {
+                updated += applyNode(projectNode, true);
+            }
+        }
+        return updated;
+    }
+
+    private int apply(String projectPath, Set<String> subPaths) throws IOException {
+        Set<String> existingPaths = directoriesPathsByProject.getOrDefault(projectPath, new HashSet<>());
+
+        Set<String> addedDirectories = difference(subPaths, existingPaths);
+        // TODO - add support for removed directories - not that simple due to current node being only one level of the tree
+
+        if (addedDirectories.isEmpty()) {
+            return 0;
+        }
+
+        Path dotSettingsFilePath = dotSettingsFilePath(projectPath);
+        String dotSettingsContent;
+        if (!new File(dotSettingsFilePath.toString()).exists()) {
+            dotSettingsContent = dotSettingsEmptyContent;
+        } else {
+            dotSettingsContent = Files.readString(dotSettingsFilePath);
+        }
+
+        int updated = 0;
+        for (String directoryPath : addedDirectories) {
+            String notNamespaceProviderLine = notNamespaceProviderLine(directoryPath);
+            if (dotSettingsContent.contains(notNamespaceProviderLine)) {
+                continue;
+            }
+            log.info(String.format("Setting directory '%s' of project '%s' as not a namespace provider", directoryPath, projectPath));
+            dotSettingsContent = dotSettingsContent.replace("</wpf:ResourceDictionary>", "\t" + notNamespaceProviderLine + "\n</wpf:ResourceDictionary>");
+            updated++;
+        }
+
+        Files.write(dotSettingsFilePath, dotSettingsContent.getBytes(StandardCharsets.UTF_8));
+
+        existingPaths.addAll(addedDirectories);
+        return updated;
+    }
+
+    private static <T> Set<T> difference(final Set<T> setOne, final Set<T> setTwo) {
+        Set<T> result = new HashSet<>(setOne);
+        result.removeIf(setTwo::contains);
+        return result;
+    }
+
+    private Path dotSettingsFilePath(String projectPath) {
+        return Paths.get(String.format("%s.DotSettings", projectPath));
+    }
+
+    private String projectPath(ProjectModelEntity node) {
+        while (node != null && !(node.getDescriptor() instanceof RdProjectDescriptor)) {
+            node = node.getParentEntity();
+        }
+        if (node == null) {
+            return null;
+        }
+        return ((RdCustomLocation) node.getDescriptor().getLocation()).getCustomLocation();
+    }
+
+    private String nodeFullRelativePath(ProjectModelEntity node) {
+        ProjectModelEntity parent = node.getParentEntity();
+        if (parent == null || !(parent.getDescriptor() instanceof RdProjectFolderDescriptor)) {
+            return node.getName();
+        }
+        return String.format("%s/%s", nodeFullRelativePath(parent), node.getName());
+    }
+
+    private String notNamespaceProviderLine(String relativeDirectoryPath) {
+        return
+            "<s:Boolean x:Key=\"/Default/CodeInspection/NamespaceProvider/NamespaceFoldersToSkip/=" +
+                relativeDirectoryPath.toLowerCase().replace(File.separator, "_005C") +
+                "/@EntryIndexedValue\">True</s:Boolean>";
+    }
+
+    private final String dotSettingsEmptyContent =
+        "<wpf:ResourceDictionary xml:space=\"preserve\" xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" xmlns:s=\"clr-namespace:System;assembly=mscorlib\" xmlns:ss=\"urn:shemas-jetbrains-com:settings-storage-xaml\" xmlns:wpf=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\">\n"
+            + "</wpf:ResourceDictionary>";
+}
--- src/main/java/com/reallyliri/plugins/namespaceproviders/NamespaceProviderSetterListener.java
+++ src/main/java/com/reallyliri/plugins/namespaceproviders/NamespaceProviderSetterListener.java
@@ -1,45 +1,61 @@
 package com.reallyliri.plugins.namespaceproviders;
 
-import com.google.common.collect.Streams;
+import com.intellij.icons.AllIcons.Actions;
 import com.intellij.ide.util.treeView.AbstractTreeNode;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.jetbrains.rider.model.RdCustomLocation;
-import com.jetbrains.rider.model.RdProjectDescriptor;
-import com.jetbrains.rider.model.RdProjectFolderDescriptor;
 import com.jetbrains.rider.model.RdSolutionDescriptor;
 import com.jetbrains.rider.projectView.views.solutionExplorer.SolutionExplorerCustomization;
 import com.jetbrains.rider.projectView.workspace.ProjectModelEntity;
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
 
 public class NamespaceProviderSetterListener extends SolutionExplorerCustomization {
     private static final Logger log = Logger.getInstance(NamespaceProviderSetterListener.class);
-    private Map<String, Set<String>> directoriesPathsByProject = new HashMap<>();
-    private Set<String> ignoredDirectoryNames = Set.of(
-        ".idea",
-        "bin",
-        "bld",
-        "build",
-        "logs",
-        "obj",
-        "target"
-    );
+    private final Applier applier = new Applier();
+    private ProjectModelEntity root;
 
     public NamespaceProviderSetterListener(@NotNull Project project) {
         // its called project, but actually its a solution, probably due to it being project in other IDEA products
         super(project);
+    }
+
+    @Override
+    public void addPrimaryToolbarActions(@NotNull DefaultActionGroup actionGroup) {
+        actionGroup.addSeparator();
+        actionGroup.addAction(new ApplyAction());
+        actionGroup.addSeparator();
+        super.addPrimaryToolbarActions(actionGroup);
+    }
+
+    private class ApplyAction extends AnAction {
+        public ApplyAction() {
+            super("Apply No-Namespace-Providers", "Apply configuration to mark all directories as non-namespace-providers", Actions.ProjectDirectory);
+        }
+
+        @Override
+        public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+            try {
+                if (root != null) {
+                    int updated = applier.apply(root, true);
+                    Notifications.Bus.notify(new Notification(
+                        Notifications.SYSTEM_MESSAGES_GROUP_ID,
+                        "No-Namespace-Providers",
+                        updated > 0 ? String.format("%d directories were updated", updated) : "No updates were detected",
+                        NotificationType.INFORMATION
+                    ));
+                }
+            } catch (Throwable ex) {
+                log.error(String.format("NamespaceProviderSetterListener error on solution %s", getProject()), ex);
+            }
+        }
     }
 
     @NotNull
@@ -57,102 +73,8 @@ public class NamespaceProviderSetterListener extends SolutionExplorerCustomizati
 
     private void listen(ProjectModelEntity parentNode) throws IOException {
         if (parentNode.getDescriptor() instanceof RdSolutionDescriptor) {
-            Streams.stream(parentNode.getChildrenEntities().iterator())
-                .filter(node -> node.getDescriptor() instanceof RdProjectDescriptor)
-                .forEach(projectNode -> {
-                    String projectPath = projectPath(projectNode);
-                    if (projectPath == null) {
-                        return;
-                    }
-                    String projectName = Paths.get(projectPath).getFileName().toString();
-                    if (!directoriesPathsByProject.containsKey(projectName)) {
-                        directoriesPathsByProject.put(projectName, new HashSet<>());
-                    }
-                });
-            return;
+            root = parentNode;
         }
-
-        String projectPath = projectPath(parentNode);
-        if (projectPath == null) {
-            return;
-        }
-        String projectName = Paths.get(projectPath).getFileName().toString();
-        if (!directoriesPathsByProject.containsKey(projectName)) {
-            return;
-        }
-        Set<String> projectDirectoriesPaths = directoriesPathsByProject.get(projectName);
-        Set<String> currentDirectoryNodesPaths =
-            Streams.stream(parentNode.getChildrenEntities().iterator())
-                .filter(node -> node.getDescriptor() instanceof RdProjectFolderDescriptor)
-                .filter(node -> !ignoredDirectoryNames.contains(node.getName().toLowerCase()))
-                .map(this::nodeFullRelativePath)
-                .collect(Collectors.toSet());
-
-        Set<String> addedDirectories = difference(currentDirectoryNodesPaths, projectDirectoriesPaths);
-        // TODO - add support for removed directories - not that simple due to current node being only one level of the tree
-
-        if (addedDirectories.isEmpty()) {
-            return;
-        }
-
-        Path dotSettingsFilePath = dotSettingsFilePath(projectPath);
-        String dotSettingsContent;
-        if (!new File(dotSettingsFilePath.toString()).exists()) {
-            dotSettingsContent = dotSettingsEmptyContent;
-        } else {
-            dotSettingsContent = new String(Files.readAllBytes(dotSettingsFilePath), StandardCharsets.UTF_8);
-        }
-
-        for (String directoryPath : addedDirectories) {
-            String notNamespaceProviderLine = notNamespaceProviderLine(directoryPath);
-            if (dotSettingsContent.contains(notNamespaceProviderLine)) {
-                continue;
-            }
-            log.info(String.format("Setting directory '%s' of project '%s' as not a namespace provider", directoryPath, getProject().getName()));
-            dotSettingsContent = dotSettingsContent.replace("</wpf:ResourceDictionary>", "\t" + notNamespaceProviderLine + "\n</wpf:ResourceDictionary>");
-        }
-
-        Files.write(dotSettingsFilePath, dotSettingsContent.getBytes(StandardCharsets.UTF_8));
-
-        projectDirectoriesPaths.addAll(addedDirectories);
+        applier.apply(parentNode, false);
     }
-
-    private static <T> Set<T> difference(final Set<T> setOne, final Set<T> setTwo) {
-        Set<T> result = new HashSet<>(setOne);
-        result.removeIf(setTwo::contains);
-        return result;
-    }
-
-    private Path dotSettingsFilePath(String projectPath) {
-        return Paths.get(String.format("%s.DotSettings", projectPath));
-    }
-
-    private String projectPath(ProjectModelEntity node) {
-        while (node != null && !(node.getDescriptor() instanceof RdProjectDescriptor)) {
-            node = node.getParentEntity();
-        }
-        if (node == null) {
-            return null;
-        }
-        return ((RdCustomLocation) node.getDescriptor().getLocation()).getCustomLocation();
-    }
-
-    private String nodeFullRelativePath(ProjectModelEntity node) {
-        ProjectModelEntity parent = node.getParentEntity();
-        if (parent == null || !(parent.getDescriptor() instanceof RdProjectFolderDescriptor)) {
-            return node.getName();
-        }
-        return String.format("%s/%s", nodeFullRelativePath(parent), node.getName());
-    }
-
-    private String notNamespaceProviderLine(String relativeDirectoryPath) {
-        return
-            "<s:Boolean x:Key=\"/Default/CodeInspection/NamespaceProvider/NamespaceFoldersToSkip/=" +
-                relativeDirectoryPath.toLowerCase().replace(File.separator, "_005C") +
-                "/@EntryIndexedValue\">True</s:Boolean>";
-    }
-
-    private final String dotSettingsEmptyContent =
-        "<wpf:ResourceDictionary xml:space=\"preserve\" xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\" xmlns:s=\"clr-namespace:System;assembly=mscorlib\" xmlns:ss=\"urn:shemas-jetbrains-com:settings-storage-xaml\" xmlns:wpf=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\">\n"
-            + "</wpf:ResourceDictionary>";
 }
